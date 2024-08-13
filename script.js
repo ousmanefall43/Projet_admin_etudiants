e7980769c565a2df2103609661b2b98746d7cfef
ousmanefall43 <119119455+ousmanefall43@users.noreply.github.com>
2024-08-13 09:05:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('adminSignupForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Envoyer les données au serveur
+});
+
+document.getElementById('adminLoginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Envoyer les données au serveur pour la connexion
+});
+
+document.getElementById('studentSignupForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Envoyer les données d'inscription étudiant au serveur
+});
+
+// Ajouter des fonctions pour gérer la liste des étudiants (non archivés et archivés)
